3b08c84718f472159aa5d869f5cfb535d534fe59
Johan Lindefelt <johan@lindefelt.io>
2025-12-04 09:03:42 +0100

    day 4 p2

--- lib/d4.ex
+++ lib/d4.ex
@@ -25,6 +25,11 @@ defmodule D4 do
         end
       end
     end
+      removed =
+    Enum.zip(List.flatten(matrix), List.flatten(new_matrix))
+    |> Enum.count(fn {old, new} -> old == "@" and new == "x" end)
+
+    {new_matrix,removed}
   end
 
   #Returns adjacent nodes
@@ -44,21 +49,35 @@ defmodule D4 do
   end
 
   defp solve_p1(matrix) do
-    new_matrix = remove_rolls(matrix,4)
-    new_matrix |> List.flatten() |> Enum.count(&(&1 == "x"))
+    {_,removed} = remove_rolls(matrix,4)
+    removed
   end
 
+  defp solve_p2(matrix) do
+      {_,removed} = remove_recur(matrix,0)
+      removed
+
+  end
+
+  defp remove_recur(matrix,removed_total)do
+    {new_matrix,removed} = remove_rolls(matrix,4)
+
+    if(removed == 0) do
+      {new_matrix,removed_total}
+    else
+      remove_recur(new_matrix,removed+removed_total)
+    end
+  end
 
   def main() do
     matrix = parse_input()
-    #val = solve_p1(matrix)
 
    {time_p1, val1} = :timer.tc(fn -> solve_p1(matrix) end)
    IO.puts("P1 removed: #{val1}")
    IO.puts("µs P1: #{time_p1}")
 
-   #{time_p2, val2} = :timer.tc(fn -> solve_p2(matrix) end)
-   #IO.puts("P1 removed: #{val2}")
-   #IO.puts("µs P1: #{time_p2}")
+   {time_p2, val2} = :timer.tc(fn -> solve_p2(matrix) end)
+   IO.puts("P2 removed: #{val2}")
+   IO.puts("µs P2: #{time_p2}")
   end
 end
