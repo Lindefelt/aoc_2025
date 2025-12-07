f4d42a5412854256d42e0d06aa6296fba9cf36a4
Johan Lindefelt <johan@lindefelt.io>
2025-12-07 21:03:13 +0100

    Day 7 part 2

--- lib/d7.ex
+++ lib/d7.ex
@@ -45,6 +45,55 @@ defmodule D7 do
   end
 
 
+ defp solve_p2(data) do
+    first_row =  data |> Enum.fetch!(0)
+    start = first_row |> Enum.find_index(fn e -> e=="S"end)
+    traverse(data,start)
+ end
+
+ defp traverse(grid,start) do
+
+    max_x = length(grid|>Enum.fetch!(0))-1
+    max_y = length(grid)-1
+
+    timelines = for x <- 0..max_x do
+      if x ==  start, do: 1, else: 0
+    end
+
+    Enum.reduce_while(0..max_y,{0,timelines}, fn y, {acc,timelines} ->
+      if y == max_y do
+        {:halt,acc+ Enum.sum(timelines)}
+      else
+        below = grid |> Enum.fetch!(y+1)
+
+        next_row =
+        Enum.reduce(0..(max_x), List.duplicate(0, max_x+1), fn x, next ->
+          c = Enum.fetch!(timelines, x)
+
+          if c == 0 do
+            next
+          else
+            case Enum.fetch!(below, x) do
+              "." ->
+                List.update_at(next, x, &(&1 + c))
+
+              "^" ->
+                next =
+                  if x - 1 >= 0, do: List.update_at(next, x - 1, &(&1 + c)), else: next
+
+                if x + 1 <= max_x, do: List.update_at(next, x + 1, &(&1 + c)), else: next
+
+              _ ->
+                next
+            end
+          end
+        end)
+
+      {:cont, {acc, next_row}}
+      end
+    end)
+  end
+
   def main() do
     data = parse_input()
 
@@ -52,8 +101,8 @@ defmodule D7 do
     IO.puts("P1: #{result_p1}")
     IO.puts("µs P1: #{time_p1}")
 
-   # {time_p2, result_p2} = :timer.tc(fn -> solve_p2(data2) end)
-   # IO.puts("P2: #{result_p2}")
-   # IO.puts("µs P2: #{time_p2}")
+    {time_p2, result_p2} = :timer.tc(fn -> solve_p2(data) end)
+    IO.puts("P2: #{result_p2}")
+    IO.puts("µs P2: #{time_p2}")
   end
 end
